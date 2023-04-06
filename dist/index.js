61416388858fd7f2b2566ab2cb188bfff87f8b5d
Vishal Mishra <vishalmishra3141005@gmail.com>
2023-04-06 16:37:46 +0530

    Added basic functionaly

--- dist/index.js
+++ dist/index.js
@@ -14,5 +14,64 @@ let plusButton = document.getElementById("+");
 let minusButton = document.getElementById("-");
 let productButton = document.getElementById("*");
 let equalButton = document.getElementById("=");
-let decimalButton = document.getElementById(".");
+let clearButton = document.getElementById("AC");
 let divideButton = document.getElementById("/");
+zeroButton === null || zeroButton === void 0 ? void 0 : zeroButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}0`;
+});
+oneButton === null || oneButton === void 0 ? void 0 : oneButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}1`;
+});
+twoButton === null || twoButton === void 0 ? void 0 : twoButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}2`;
+});
+threeButton === null || threeButton === void 0 ? void 0 : threeButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}3`;
+});
+fourButton === null || fourButton === void 0 ? void 0 : fourButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}4`;
+});
+fiveButton === null || fiveButton === void 0 ? void 0 : fiveButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}5`;
+});
+sixButton === null || sixButton === void 0 ? void 0 : sixButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}6`;
+});
+sevenButton === null || sevenButton === void 0 ? void 0 : sevenButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}7`;
+});
+eightButton === null || eightButton === void 0 ? void 0 : eightButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}8`;
+});
+nineButton === null || nineButton === void 0 ? void 0 : nineButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}9`;
+});
+plusButton === null || plusButton === void 0 ? void 0 : plusButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}+`;
+});
+minusButton === null || minusButton === void 0 ? void 0 : minusButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}-`;
+});
+productButton === null || productButton === void 0 ? void 0 : productButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}*`;
+});
+divideButton === null || divideButton === void 0 ? void 0 : divideButton.addEventListener("click", function (e) {
+    outputDisplay.value = `${outputDisplay.value}/`;
+});
+clearButton === null || clearButton === void 0 ? void 0 : clearButton.addEventListener("click", function (e) {
+    outputDisplay.value = "";
+});
+equalButton === null || equalButton === void 0 ? void 0 : equalButton.addEventListener("click", function (e) {
+    let result = evaluate(outputDisplay.value);
+    outputDisplay.value = result;
+});
+function evaluate(s) {
+    let ans;
+    try {
+        ans = eval(s);
+    }
+    catch (err) {
+        return "Invalid";
+    }
+    return eval(s);
+}
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 
-let outputDisplay = document.getElementById("output");
+
+let outputDisplay = document.getElementById("output") as HTMLInputElement;
 
 let zeroButton = document.getElementById("0");
 let oneButton = document.getElementById("1");
@@ -16,6 +17,82 @@ let plusButton = document.getElementById("+");
 let minusButton = document.getElementById("-");
 let productButton = document.getElementById("*");
 let equalButton = document.getElementById("=");
-let decimalButton = document.getElementById(".");
+let clearButton = document.getElementById("AC");
 let divideButton = document.getElementById("/");
 
+zeroButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}0`; 
+});
+
+oneButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}1`; 
+});
+
+twoButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}2`; 
+});
+
+threeButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}3`; 
+});
+
+fourButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}4`
+});
+
+fiveButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}5`
+});
+
+sixButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}6`
+});
+
+sevenButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}7`
+});
+
+eightButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}8`
+});
+
+nineButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}9`
+});
+
+plusButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}+`
+});
+
+
+minusButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}-`
+});
+
+productButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}*`
+});
+
+divideButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = `${outputDisplay!.value}/`
+});
+
+clearButton?.addEventListener("click", function(e) {
+    outputDisplay!.value = "";
+});
+
+equalButton?.addEventListener("click", function(e) {
+    let result: string = evaluate(outputDisplay!.value);
+    outputDisplay!.value = result
+});
+
+
+function evaluate(s: string): string {
+    let ans: string;
+    try {
+        ans = eval(s);
+    } catch(err) {
+        return "Invalid";
+    }
+    return eval(s);
+}
